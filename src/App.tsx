f8cdba9a1a403c97c59bc11054b5ab9137bc7a22
Krupa Patel <patelkrupa0425@gmail.com>
2025-03-20 11:23:19 +0530

    add rout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 // import { useState } from 'react'
+import { BrowserRouter, Route , Routes } from 'react-router-dom'
 import './App.css'
 
 import ForgotPasswordPage from './ForgotPages/ForgotPage'
-// import LoginPage from './Login Pages/LoginPage'
-// import SignUpPage from './Login Pages/SignUpPage'
-// import OTPPage from './ForgotPages/OPTpage'
-// import ResetPage from './ForgotPages/ResetPage'
-// import NGOProfilePage from './NGO Profile Page/NGOProfilePage'
+import LoginPage from './Login Pages/LoginPage'
+import SignUpPage from './Login Pages/SignUpPage'
+import OTPPage from './ForgotPages/OPTpage'
+import ResetPage from './ForgotPages/ResetPage'
+import NGOProfilePage from './NGO Profile Page/NGOProfilePage'
 
 function App() {
 
   return (
-    <>
-     {/* <LoginPage /> */}
-     <ForgotPasswordPage />
-      {/* <SignUpPage /> */}
-      {/* < OTPPage /> */}
-      {/* <ResetPage/> */}
-      {/* <NGOProfilePage/> */}
-    </>
+    <BrowserRouter>
+    <Routes>
+          <Route path='/login' element={<LoginPage />}/>
+          <Route path='/sign' element={<SignUpPage />}/>
+          <Route path='/forgot' element={<ForgotPasswordPage />}/>
+          <Route path='/otp' element={<OTPPage />}/>
+          <Route path='/reset' element={<ResetPage />}/>
+          <Route path='/ngoprofile' element={<NGOProfilePage/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
