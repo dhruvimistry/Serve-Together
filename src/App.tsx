a46b2081e89cfb80d1b6fcf5890fc7c95cc90d1a
HETVI BAROT <hetvibarot245@gmail.com>
2025-03-20 10:59:29 +0530

    made homapage responsive

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@
 import './App.css'
 import Homepage from './Homepage/Homepage'
 // import LoginPage from './pages/LoginPage'
+// import Profile from './Volprofile/Volprofileviewer'
+
+
 
 function App() {
 
@@ -9,6 +12,7 @@ function App() {
     <>
      {/* <LoginPage /> */}
      <Homepage />
+     {/* <Profile /> */}
     </>
   )
 }
