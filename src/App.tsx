a61b52249f2b2270ff11457d37eec48302a861f9
Dhruvi Mistry <dhruvim433@gmail.com>
2025-03-21 11:33:56 +0530

    Changed Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
-// import { useState } from 'react'
-import { BrowserRouter, Route , Routes } from 'react-router-dom'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
 
-import ForgotPasswordPage from './ForgotPages/ForgotPage'
-import LoginPage from './Login Pages/LoginPage'
-import SignUpPage from './Login Pages/SignUpPage'
-import HomePage from './Home'
-import OTPPage from './ForgotPages/OPTpage'
-import ResetPage from './ForgotPages/ResetPage'
-import NGOProfilePage from './NGO Profile Page/NGOProfilePage'
+import ForgotPasswordPage from './ForgotPages/ForgotPage';
+import LoginPage from './Login Pages/LoginPage';
+import SignUpPage from './Login Pages/SignUpPage';
+import HomePage from './Home';
+import OTPPage from './ForgotPages/OPTpage';
+import ResetPage from './ForgotPages/ResetPage';
+import NGOProfilePage from './NGO Profile Page/NGOProfilePage';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  { path: '/login', element: <LoginPage /> },
+  { path: '/signup', element: <SignUpPage /> },
+  { path: '/forgot-password', element: <ForgotPasswordPage /> },
+  { path: '/otp', element: <OTPPage /> },
+  { path: '/reset-password', element: <ResetPage /> },
+  { path: '/ngo-profile', element: <NGOProfilePage /> },
+]);
 
 function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-       <Route path='/' element={<HomePage />}/>
-        <Route path='/login' element={<LoginPage />}/>
-        <Route path='/signup' element={<SignUpPage />}/>
-        <Route path='/forgot-password' element={<ForgotPasswordPage />}/>
-        <Route path='/otp' element={<OTPPage />}/>
-        <Route path='/reset-password' element={<ResetPage />}/>
-        <Route path='/ngo-profile' element={<NGOProfilePage/>}/>
-      </Routes>
-    </BrowserRouter>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
