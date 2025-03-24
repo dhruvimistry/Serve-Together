4278f50a29cc86e2513299a43e50148397a6477d
Krupa Patel <patelkrupa0425@gmail.com>
2025-03-24 19:51:28 +0530

    Navbar with offcanvas responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,56 +1,37 @@
 import React from "react";
 import HeadLogo from "../assets/serve-together-1.png";
-import "bootstrap/dist/css/bootstrap.min.css"; 
+import "bootstrap/dist/css/bootstrap.min.css";
 import { FaUserCircle } from "react-icons/fa";
+import { Navbar, Nav, Container, Offcanvas } from "react-bootstrap";
 
 const NavBar: React.FC = () => {
   return (
-    <nav className="navbar navbar-expand-lg background">
-      <div className="container">
-        <a className="navbar-brand" href="#home">
+    <Navbar expand="lg" className="background">
+      <Container>
+        <Navbar.Brand href="#home">
           <img src={HeadLogo} width="200" alt="ServeTogether Logo" />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link theme" href="#home">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link theme" href="#about">About Us</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link theme" href="#events">Events</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link theme" href="#volunteers">Volunteers</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link theme" href="#requests">Requests</a>
-            </li>
-            <a href="#" className="nav-link">
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Offcanvas id="offcanvasNavbar" placement="end">
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Menu</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className="theme">Home</Nav.Link>
+              <Nav.Link href="#about" className="theme">About Us</Nav.Link>
+              <Nav.Link href="#events" className="theme">Events</Nav.Link>
+              <Nav.Link href="#volunteers" className="theme">Volunteers</Nav.Link>
+              <Nav.Link href="#requests" className="theme">Requests</Nav.Link>
+              <Nav.Link href="#">
                 <FaUserCircle size={24} className="text-primary" />
-            </a>
-          </ul>
-          {/* <div className="d-flex gap-2 ms-3">
-            <button className="btn theme-bg">Login</button>
-            <button className="btn theme-bg">Sign Up</button>
-          </div> */}
-        </div>
-      </div>
-    </nav>
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
   );
 };
 
 export default NavBar;
- 
