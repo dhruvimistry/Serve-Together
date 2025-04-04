741fe9975ba23012eafa303e2825caa114982392
Krupa Patel <patelkrupa0425@gmail.com>
2025-04-04 11:35:47 +0530

    changes EventsCard

--- src/components/common/EventsCard.tsx
+++ src/components/common/EventsCard.tsx
@@ -16,11 +16,33 @@ const EventCard: React.FC<EventCardProps> = ({ nameOfOrganization, title, locati
       <div className="card p-4 shadow-sm common-radius">
         <h5 className="card-title">{ nameOfOrganization }</h5>
         <p className="card-text ">
-          <span role="img" aria-label="event">⭐</span> {title} <br />
-          <span role="img" aria-label="location">📍</span> {location} <br />
-          <span role="img" aria-label="calendar">📅</span> {date} |
-          <span role="img" aria-label="clock">⏰</span> {time} <br />
-          <span role="img" aria-label="skills">🎯</span> {skills}
+        <table className="table table-borderless mb-0">
+          <tr>
+          <td><span role="img" aria-label="event">⭐</span></td>
+          <td>{title}</td>
+          </tr>
+          <tr>
+          <td><span role="img" aria-label="location">📍</span></td>
+          <td>{location}</td>
+          </tr>
+          <tr>
+          <td><span role="img" aria-label="calendar">📅</span></td>
+          <td>{date}</td>
+          </tr>
+          <tr>
+          <td><span role="img" aria-label="clock">⏰</span></td>
+          <td>{time}</td>
+          </tr>
+          <tr>
+          <td><span role="img" aria-label="skills">🎯</span></td>
+          <td>{skills}</td>
+          </tr>
+        </table>
+          {/* <span role="img" aria-label="event">⭐</span> {title} <br /> */}
+          {/* <span role="img" aria-label="location">📍</span> {location} <br /> */}
+          {/* <span role="img" aria-label="calendar">📅</span> {date} | */}
+          {/* <span role="img" aria-label="clock">⏰</span> {time} <br /> */}
+          {/* <span role="img" aria-label="skills">🎯</span> {skills} */}
         </p>
         <div className="d-flex justify-content-center gap-3">
           <CustomButton label="Apply" variant="primary" width="w-50"/>
