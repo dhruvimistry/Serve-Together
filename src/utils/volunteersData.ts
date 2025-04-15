ffd5bf31d692bd024daf140d1b6d6a9177bca9ef
Dhruvi Mistry <dhruvim433@gmail.com>
2025-04-15 23:15:20 +0530

    Replaced city names with origi

--- src/utils/volunteersData.ts
+++ src/utils/volunteersData.ts
@@ -1,10 +1,10 @@
 const volunteersData = [
-  { name: "Aarav Mehta", skills: "Teaching, Communication, Patience", cities: "City1, City2" },
-  { name: "Riya Sharma", skills: "Painting, Crafting, Creativity", cities: "City1, City2" },
-  { name: "Vihaan Iyer", skills: "Coaching, Motivation, Teamwork", cities: "City1, City2" },
-  { name: "Zoya Khan", skills: "Teamwork, Environmental Awareness", cities: "City1, City2" },
-  { name: "Kabir Joshi", skills: "Singing, Dancing, Instrumental Skill", cities: "City1, City2" },
-  { name: "Myra Patel", skills: "Public Speaking, Storytelling, Communication", cities: "City1, City2" },
+  { name: "Aarav Mehta", skills: "Teaching, Communication, Patience", cities: "Ahmedabad, Surat" },
+  { name: "Riya Sharma", skills: "Painting, Crafting, Creativity", cities: "Rajkot, Jamnagar" },
+  { name: "Vihaan Iyer", skills: "Coaching, Motivation, Teamwork", cities: "Vadodara, Bhavnagar" },
+  { name: "Zoya Khan", skills: "Teamwork, Environmental Awareness", cities: "Gandhinagar, Junagadh" },
+  { name: "Kabir Joshi", skills: "Singing, Dancing, Instrumental Skill", cities: "Nadiad, Morbi" },
+  { name: "Myra Patel", skills: "Public Speaking, Storytelling, Communication", cities: "Bharuch, Porbandar" },
 ];
 
 export default volunteersData;
